91d2df5c45a9e22b63a3c41f447866b56210d0c3
Brenda Lau <bnilau@gmail.com>
2018-08-09 16:30:57 -0400

    corrected URL typo

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname + '/dist/cs4550-summer2-18-angular-blau'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/cs4550-summer2-18-angular-blaur/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/cs4550-summer2-18-angular-blau/index.html'));
 });
 
 app.listen(process.env.PORT || 4200);
