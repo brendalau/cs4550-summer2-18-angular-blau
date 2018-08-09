d90183b344b7175ed2c503d2e9d286cdb5f9a592
Brenda Lau <bnilau@gmail.com>
2018-08-09 16:30:00 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/cs4550-summer2-18-angular-blau'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/cs4550-summer2-18-angular-blaur/index.html'));
+});
+
+app.listen(process.env.PORT || 4200);
